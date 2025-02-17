7f5eea8732b6860a1d7cdf0e51a8c80a802a3399
Seung Hwang <2018066@naver.com>
2025-02-17 20:50:38 +0900

    다시 만들었음

--- main.js
+++ main.js
@@ -1,86 +1,78 @@
-//랜던번호 지정
-//유저가 번호를 입력한다 그리고 go라는 버튼을 누름
-//만약에 유저가 랜덤번호를 맞추면 맞췄습니다!
-//핸덤번호가 < 유전번호 Down
-//랜덤번호가 > 유저번호 Up
-//reset버튼을 누르면 게임이 리셋된다
-//5번의 기회를 다 쓰면 게임이 끝난다(더 이상 추측 불가, 버튼이disable)
-//유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. 기회를 깎지 않는다
-//유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깎지 않는다.
+//1~100까지 랜덤한 숫자가 정해져야함
+//숫자를 입력할 창과 입력할 버튼 있어야함
+//결과값이 보여야 함
+//유저가 입력한 값 > 정답 Down
+//유저가 입력한 값 < 정답 Up
+//정답을 맞추면 맞췄다고 알려주기
+//Reset버튼을 누르면 다시 랜덤한 숫자 생성, 기회 초기화
+//1~100사이의 숫자 외에 다른 숫자를 입력하면 알려줘야함 기회를 깎지않음
+//기회는 5번 제한 5번이 끝나면 버튼 비활성화
+//중복된 숫자를 입력하면 알려주기 기회 깎이지 않음
 
-let computerNum = 0;
-let playButton = document.getElementById("play-button");
-let userInput = document.getElementById("user-input");
-let resultArea = document.getElementById("result-area");
-let resetButton = document.getElementById("reset-button");
+let randomNum = 0;
 let chances = 5;
-let gameOver = false;
+let inputList = []; //입력한 숫자를 저장하는 배열
+let gameOver = false; //go버튼 비활성화 여부
+let inputNum = document.getElementById("input-number");
+let goButton = document.getElementById("go-button");
+let resultArea = document.getElementById("result-area");
 let chanceArea = document.getElementById("chance-area");
-let history = [];
+let resetButton = document.getElementById("reset-button");
 
-playButton.addEventListener("click", play);
+goButton.addEventListener("click", go);
 resetButton.addEventListener("click", reset);
-userInput.addEventListener("focus", function () {
-  userInput.value = "";
+inputNum.addEventListener("focus", function () {
+  inputNum.value = "";
 });
 
-function pickRandomNum() {
-  computerNum = Math.floor(Math.random() * 100) + 1;
-  console.log("정답", computerNum);
+function randomNumber() {
+  //1~100 랜덤 숫자 생성하는 함수
+  randomNum = Math.floor(Math.random() * 100) + 1;
+  console.log("정답", randomNum);
 }
+randomNumber(); //랜덤 숫자 생성
 
-function play() {
-  let userValue = userInput.value;
-
-  if (userValue < 1 || userValue > 100) {
-    resultArea.textContent = "1과 100 사이의 숫자를 입력해 주세요.";
+function go() {
+  let userNum = inputNum.value;
+  if (userNum < 1 || userNum > 100) {
+    //입력값이 1~100 범위를 벗어나면 알려주면서 종료
+    resultArea.textContent = "1부터 100 사이의 값을 입력해 주세요.";
     return;
   }
 
-  if (history.includes(userValue) == true) {
-    resultArea.textContent =
-      "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요.";
-    return;
-  }
-
-  chances--;
-  chanceArea.textContent = `남은기회 : ${chances}번`;
-  console.log("chance", chances);
-
-  if (userValue < computerNum) {
-    resultArea.textContent = "Up!!";
-  } else if (userValue > computerNum) {
-    resultArea.textContent = "Down!!";
+  if (userNum < randomNum) {
+    resultArea.textContent = "Up!";
+  } else if (userNum > randomNum) {
+    resultArea.textContent = "Down!";
   } else {
     resultArea.textContent = "정답입니다!!";
-    gameOver = true;
   }
 
-  history.push(userValue);
-  console.log(history);
+  chances--; //기회 차감
+  chanceArea.textContent = `기회 : ${chances}번`;
+
+  if (inputList.includes(userNum) == true) {
+    resultArea.textContent = "중복된 숫자입니다. 다시 입력해 주세요";
+    return;
+  } else {
+    inputList.push(userNum); //배열에 입력값을 추가
+    console.log(inputList);
+  }
 
   if (chances < 1) {
     gameOver = true;
   }
   if (gameOver == true) {
-    playButton.disabled = true;
+    goButton.disabled = gameOver; //go버튼 비활성화
   }
 }
 
 function reset() {
-  //user input 창이 깨끗하게 정리되고
-  userInput.value = "";
-  // 새로운 번호가 생성되고
-  pickRandomNum();
-  // 결과창 초기화
-  resultArea.textContent = "결과값이 여기 나옵니다";
-  //go버튼 활성화
-  playButton.disabled = false;
-  //기회 초기화
-  chances = 5;
-  chanceArea.textContent = `남은기회 : ${chances}번`;
-  //history 초기화
-  history = [];
+  chances = 5; //기회 초기화
+  inputList = []; //입력값 배열 초기화
+  inputNum.value = ""; //input창 공백
+  resultArea.textContent = "범위는 1부터 100까지입니다";
+  chanceArea.textContent = `기회 : ${chances}번`;
+  randomNumber(); //랜덤 숫자 재생성
+  gameOver = false; //go버튼 활성화
 }
-
-pickRandomNum();
